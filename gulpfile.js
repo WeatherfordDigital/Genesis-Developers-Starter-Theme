6162d4eba7c940455fd08ccf5b0242cf05955869
Tonya <hellofromtonya@thewpdc.com>
2016-07-21 14:44:36 -0500

    postcss, autoprefixing, mqpacker, sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,39 @@ var gulp = require('gulp'),
 	bourbon = require('bourbon').includePaths,
 	neat = require('bourbon-neat').includePaths,
 	sass = require('gulp-sass'),
-	autoprefixer = require('autoprefixer');
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssMQpacker = require('css-mqpacker'),
+	sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function(){
 
 	return gulp.src('assets/sass/style.scss')
-				.pipe( sass({
-					includePaths: [].concat( bourbon, neat ),
-					errLogToConsole: true,
-					outputStyle: 'expanded' // Options: nested, expanded, compact, compressed
-				}))
-				.pipe(gulp.dest('./'));
+
+	           // Wrap tasks in a sourcemap
+			.pipe( sourcemaps.init())
+
+			.pipe( sass({
+				includePaths: [].concat( bourbon, neat ),
+				errLogToConsole: true,
+				outputStyle: 'expanded' // Options: nested, expanded, compact, compressed
+			}))
+
+		   .pipe( postcss([
+			   autoprefixer({
+				   browsers: ['last 2 versions']
+			   }),
+			   cssMQpacker({
+			   	    sort: true
+			   })
+		   ]))
+
+			// creates the sourcemap
+			.pipe(sourcemaps.write())
+
+			.pipe(gulp.dest('./'));
 
 });
-
-gulp.task('autoprefixer', function() {
-
-	return gulp.src( './style.css' )
-		pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('dist'));
-});
-
 
 gulp.task('watch', function () {
 	gulp.watch('assets/sass/**/*.scss', ['styles']);
