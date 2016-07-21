6d4be12af177e60c0dcb8c55c2e6f832e3e221dd
Tonya <hellofromtonya@thewpdc.com>
2016-07-21 00:59:30 -0500

    doh, base line height is not pixels

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,29 @@
 var gulp = require('gulp'),
 	bourbon = require('bourbon').includePaths,
 	neat = require('bourbon-neat').includePaths,
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	autoprefixer = require('autoprefixer');
 
 gulp.task('styles', function(){
 
 	return gulp.src('assets/sass/style.scss')
-		.pipe( sass({
-			includePaths: [].concat( bourbon, neat ),
-			errLogToConsole: true,
-			outputStyle: 'expanded' // Options: nested, expanded, compact, compressed
-		}))
-		.pipe(gulp.dest('./'));
+				.pipe( sass({
+					includePaths: [].concat( bourbon, neat ),
+					errLogToConsole: true,
+					outputStyle: 'expanded' // Options: nested, expanded, compact, compressed
+				}))
+				.pipe(gulp.dest('./'));
 
+});
+
+gulp.task('autoprefixer', function() {
+
+	return gulp.src( './style.css' )
+		pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('dist'));
 });
 
 
